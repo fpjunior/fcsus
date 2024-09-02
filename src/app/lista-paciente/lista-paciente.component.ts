78568c400135d8791abc6be80cbf47066a9052ea
ivsonramos <ivson.ramos@gmail.com>
2024-09-02 00:03:11 -0300

    :construction:Adiciona dados a lista de usuarios

--- src/app/lista-paciente/lista-paciente.component.ts
+++ src/app/lista-paciente/lista-paciente.component.ts
@@ -6,5 +6,11 @@ import { Component } from '@angular/core';
   styleUrl: './lista-paciente.component.scss'
 })
 export class ListaPacienteComponent {
+  usuarios = [
+    {usuario: "João da Silva", cpf:"123.456.789-90", dtnasnimento: "01/01/2000", nSUS:"12345678"},
+    {usuario: "Marcos Ferreira", cpf:"456.123.789-90", dtnasnimento: "10/01/2012", nSUS:"12345678"},
+    {usuario: "Jeremias Azevedo", cpf:"789.123.456-90", dtnasnimento: "20/01/2011", nSUS:"12345678"},
+
+  ]
 
 }
